b4434f1b9f9ef0466736b170d489d0096e73e6b3
WizMe-M <enderman.201@mail.ru>
2022-03-16 10:37:22 +0300

    Added converter for header and fixed view in MainWindow.axaml

---
+++ Concerter/Converters/NotProfileConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Concerter.ViewModels;
+using Concerter.Views;
+
+namespace Concerter.Converters;
+
+public class NotProfileConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var isProfile = value is MyProfileViewModel;
+        return !isProfile;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
